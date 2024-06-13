c363504fddfcc2aeb77105e7fb2fa082142b8407
Nsikak <tm260715@gmail.com>
2024-06-13 19:30:01 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_count;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	left_count = binary_tree_height(tree->left);
 	right_count = binary_tree_height(tree->right);
 	if (left_count > right_count)
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_is_full - checks if binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return:
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	
+}
